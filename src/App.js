d285556bab1241f09e3e3d87e5a6289c85ba216a
gunasgs <gunasgs357@gmail.com>
2022-03-17 19:17:56 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./card.js";
 
 function App() {
+  let data = [
+  {
+id :1,
+plan :"free",
+cost :0,
+user :"single user",
+storage :"5GB storage",
+},
+      {
+ id :2,
+ plan :"plus",
+ cost :9,
+ user :"5 Users",
+ storage :"50GB Storage",
+ },
+{
+   id :3,
+ plan :"Pro",
+ cost :49,
+ storage:"150GB Storage",
+ }];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+     <section className="pricing py-5">
+  <div className="container">
+    <div className="row">
+   
+    {
+      data.map((card) =>
+      <Card carddata={card}/>)
+    }
+      </div>
     </div>
+    </section>
+   </>
   );
 }
 
